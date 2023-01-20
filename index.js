1cd797373cd17c842b26691c9c99913b3a3ea68b
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-01-20 12:25:54 -0600

    Changed recipes on page to an object-oriented approach

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ ingredient1 = document.querySelectorAll('.ing1');
 ingredient2 = document.querySelectorAll('.ing2');
 
 
-console.log(getDefaultRecipes());
 //Retrieves default recipes from API when page is loaded in
 async function getDefaultRecipes(){
     let recipesList = []
@@ -20,35 +19,40 @@ async function getDefaultRecipes(){
         let recipeDict = {}
         recipeDict['Name'] = hit.recipe.label;
         recipeDict['Image'] = hit.recipe.image;
-        recipeDict['cuisineType'] = hit.recipe.cuisineType;
-        recipeDict['Calories'] = hit.recipe.calories;
+        recipeDict['Cuisinetype'] = hit.recipe.cuisineType;
+        recipeDict['calories'] = hit.recipe.calories;
         recipeDict['Mealtype'] = hit.recipe.mealType;
         recipeDict['Dishtype'] = hit.recipe.dishType; //Take a look at this
-        console.log(hit.recipe.dishType);
         //Here make it so that / is changed to ' or ' and , is switched to ' ' in Mealtype
         recipesList.push(recipeDict);
     }
     return recipesList;
 }
 
-function SetRecipeInformation(){
-    //Defines recipe list
-    let recipeList;
-    //Currently generates randomly, can be adjusted later
+function setupCardClasses(){
+    //Currently generates randomly, can be adjusted later --> This can me modified by changing function below
     getDefaultRecipes().then(recipes => {
-        //For each recipe, sets proper information
+        //Iterates over each recipe provided by API, sets it up with elements in a Card object.
         let count = 0;
+
+        let cardsDisplayed = [] //List representing cards currently displayed on screen
+
         recipes.forEach(recipe => {
-            title_h3[count].innerText = recipe.Name;
-            recipe_images[count].style.backgroundImage = `url(${recipe.Image})`;
-            mealtype_text[count].innerText = recipe.cuisineType;
-            calories[count].innerText = Math.floor(recipe.Calories);
-            enjoy_your[count].innerText = `Enjoy your ${recipe.Mealtype}!`;
-            ingredient1[count].innerText = 'Category:'
-            ingredient2[count].innerText = recipe.Dishtype;
+            elementdict = {}
+            elementdict['title_h3'] = title_h3[count];
+            elementdict['recipe_image'] = recipe_images[count];
+            elementdict['mealtype_text'] = mealtype_text[count];
+            elementdict['calories'] = calories[count];
+            elementdict['enjoy_your'] = enjoy_your[count];
+            elementdict['ingredient1'] = ingredient1[count];
+            elementdict['ingredient2'] = ingredient2[count];
             
-            
+            cardsDisplayed.push(new Card(recipe, elementdict))
+
             count += 1;
+        })
+        cardsDisplayed.forEach(card => {
+            card.setRecipeInformation();
         })
     })
     .catch(error => {
@@ -56,5 +60,39 @@ function SetRecipeInformation(){
     });
 }
 
-SetRecipeInformation();
+setupCardClasses();
 
+class Card {
+    constructor(recipeDict, elementsDict){
+        
+        // Recipe Information
+        this.name = recipeDict.Name;
+        this.image = recipeDict.Image;
+        this.cuisineType = recipeDict.Cuisinetype;
+        this.calories = recipeDict.calories;
+        this.mealtype = recipeDict.Mealtype;
+        this.dishtype = recipeDict.Dishtype;
+
+        // Element Information
+        this.div = elementsDict.col;
+        this.titleText = elementsDict.title_h3;
+        this.recipeImage = elementsDict.recipe_image;
+        this.mealtypeText = elementsDict.mealtype_text;
+        this.caloriesText = elementsDict.calories;
+        this.enjoyYour = elementsDict.enjoy_your;
+        this.ingredient1 = elementsDict.ingredient1; //Note that this is actually just the category, not ingredients.
+        this.ingredient2 = elementsDict.ingredient2;
+
+    }
+
+    setRecipeInformation(){
+        //Sets the recipe up on the card
+        this.titleText.innerText = this.name;
+        this.recipeImage.style.backgroundImage = `url(${this.image})`;
+        this.mealtypeText.innerText = this.cuisineType;
+        this.caloriesText.innerText = Math.floor(this.calories);
+        this.enjoyYour.innerText = `Enjoy your ${this.mealtype}!`;
+        this.ingredient1.innerText = 'Category:';
+        this.ingredient2.innerText = this.dishtype;
+    }
+}
