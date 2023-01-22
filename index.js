778bc68f56cf3566892197d4a572a16ef49d4a7c
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-01-21 18:20:32 -0600

    made a navigation bar and made both bars responsive

--- index.js
+++ index.js
@@ -177,3 +177,42 @@ class Card {
     }
 
 }
+
+// Website responsiveness code
+document.addEventListener("DOMContentLoaded", function() {
+    var newSelect;
+    var mediaQuery = window.matchMedia("(max-width: 900px)");
+    if (mediaQuery.matches) {
+        newSelect = createNewSelect();
+    }
+    mediaQuery.addListener(function(changed) {
+        if (changed.matches) {
+            newSelect = createNewSelect();
+        } else {
+            if (newSelect) {
+                var navselector = document.getElementById("navselector");
+                navselector.removeChild(newSelect);
+                newSelect = null;
+            }
+        }
+    });
+    
+    function createNewSelect() {
+        var newSelect = document.createElement("select");
+        var homeOption = document.createElement("option");
+        var homeLink = document.getElementById("home").querySelector("a");
+        homeOption.appendChild(homeLink.cloneNode(true));
+        newSelect.appendChild(homeOption);
+        var deckOption = document.createElement("option");
+        var deckLink = document.getElementById("deck").querySelector("a");
+        deckOption.appendChild(deckLink.cloneNode(true));
+        newSelect.appendChild(deckOption);
+        var signinOption = document.createElement("option");
+        var signinLink = document.getElementById("signin").querySelector("a");
+        signinOption.appendChild(signinLink.cloneNode(true));
+        newSelect.appendChild(signinOption);
+        var navselector = document.getElementById("navselector");
+        navselector.appendChild(newSelect);
+        return newSelect;
+    }
+});
